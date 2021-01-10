51c5202fe958c099b2faf5a271803b3c3120af22
KubicekPetr <kubicekpetr@outlook.com>
2021-01-10 18:34:51 +0100

    I passed currentUser to header and implemented firebase's signOut into navigation. I utilized popup instead of redirect to login bcs it seems to be more smooth

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
+import { auth } from '../../firebase/firebase.utils';
+
+const Header = ({ currentUser }) => (
     <div>
         <Link to="/">Home</Link>
         <span>|</span>
         <Link to="/shop">Shop</Link>
-        <span>|</span>
-        <Link to="/sign-in">Sign in</Link>
-        <span>|</span>
-        <Link to="/sign-up">Sing up</Link>
+        {
+            currentUser ?
+                <Link to="#" onClick={() => auth.signOut()}>|Sign out</Link> :
+                <span>
+                    |<Link to="/sign-in">Sign in</Link>
+                    |<Link to="/sign-up">Sing up</Link>
+                </span>
+        }
     </div>
 );
 
