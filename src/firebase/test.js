a236f39cd7a04859db7ee168852c7faf6a86f8ef
KubicekPetr <kubicekpetr@outlook.com>
2021-01-11 23:58:02 +0100

    removing test file, it won't be used

--- src/firebase/test.js
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-
-const firestore = firebase.firestore();
-
-firestore.collection('users').doc('rjcdS8wnBkec650cyDb9').collection('cartItems').doc('WSRlvsCAtvpW9QjpPOqx');
-firestore.doc('/users/rjcdS8wnBkec650cyDb9/cartItems/WSRlvsCAtvpW9QjpPOqx');
