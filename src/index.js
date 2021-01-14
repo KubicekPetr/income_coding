31a13e184c150cce4c7b7057884f4557d94834a6
KubicekPetr <kubicekpetr@outlook.com>
2021-01-14 23:11:03 +0100

    installing redux

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
