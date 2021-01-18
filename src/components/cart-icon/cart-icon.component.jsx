c9fc8949a3439d5aed872725873c2c0a2df801ae
KubicekPetr <kubicekpetr@outlook.com>
2021-01-18 20:05:05 +0100

    I created new cart-reducer WITHOUT payload, cause it is just toggler and payload is optional. From cart-icon component I made toggler that listens on click and dispatches the cart action, toggle the hidden value. The value is after that used in header component on cart dropdown to toggle its visibility.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-const CartIcon = () => (
-    <div>
+const CartIcon = ({ toggleCartHidden }) => (
+    <div onClick={toggleCartHidden}>
         <ShoppingIcon style={{ width: '24px', height: '24px' }} />
         <span>0</span>
     </div>
 );
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);
