e35b9f53c72d52efb9d1c5948f0c185ab38b8a3b
KubicekPetr <kubicekpetr@outlook.com>
2021-01-18 20:18:22 +0100

    adding button to collection item, it will be responsible for adding items to cart dropdown

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Button from '../button/button.component';
+
 const CollectionItem = ({name, price}) => (
     <div>
         <span>{name}</span>
         <span>- {price}$</span>
+        <Button>Add to cart</Button>
     </div>
 );
 
