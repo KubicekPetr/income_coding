7ec6046173c5e43bb79903c69e7b5f5b22ecea55
KubicekPetr <kubicekpetr@outlook.com>
2021-01-19 23:33:19 +0100

    I created with redux state for items in cart and the action is dispatch by button in collection item that passes the whole item as payload.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
+
 import Button from '../button/button.component';
 
-const CollectionItem = ({name, price}) => (
-    <div>
-        <span>{name}</span>
-        <span>- {price}$</span>
-        <Button>Add to cart</Button>
-    </div>
-);
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price } = item;
+    return (
+        <div>
+            <span>{name}</span>
+            <span>- {price}$</span>
+            <Button onClick={() => addItem(item)}>Add to cart</Button>
+        </div>
+    );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
