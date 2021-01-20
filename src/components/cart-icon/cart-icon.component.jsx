beed4fab96b17758182ae47e9bb73f512eff98f5
KubicekPetr <kubicekpetr@outlook.com>
2021-01-20 21:28:51 +0100

    map state to cart icon and accumulate number of items in cart to show the total number of them

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,15 +5,19 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div onClick={toggleCartHidden}>
         <ShoppingIcon style={{ width: '24px', height: '24px' }} />
-        <span>0</span>
+        <span>{itemCount}</span>
     </div>
 );
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+});
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
