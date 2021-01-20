6594dfc694a20976c2a7e9a2fad6a3b73c6c4863
KubicekPetr <kubicekpetr@outlook.com>
2021-01-20 20:40:39 +0100

    refactoring

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,13 +1,12 @@
 export const addNewOrAdditionalItemToCart = (cartItems, newItem) => {
     const itemExists = cartItems.find(item => item.id === newItem.id);
     if (itemExists) {
-        return [
-            ...cartItems.filter(item => item.id !== newItem.id),
-            {
-                ...itemExists,
-                ...{ quantity: itemExists.quantity + 1 },
-            },
-        ];
+        return cartItems.map(item => 
+            item.id === newItem.id 
+            ? {...item, quantity: itemExists.quantity + 1} 
+            : item
+        );
+
     }
 
     return [...cartItems, { ...newItem, quantity: 1 }];
