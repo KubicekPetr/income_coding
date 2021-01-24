1bc39c151295ffba7f0e4f6ef563b8f0af6766f4
KubicekPetr <kubicekpetr@outlook.com>
2021-01-24 14:32:49 +0100

    making checkout table on checkout page with all chosen products and creating selector for total sum to pay

--- src/pages/checkout/checkout.page.jsx
+++ src/pages/checkout/checkout.page.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
-const Checkout = () => (
-    <div>Checkout page</div>
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+
+const Checkout = ({ cartItems, total }) => (
+    <div>
+        <h1>Cart checkout</h1>
+        <table style={{borderSpacing: '10px', textAlign: 'center'}}>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Remove</th>
+            {cartItems.map(item => (
+                <tr>
+                    <td>{item.name}</td>
+                    <td>{item.quantity}</td>
+                    <td>{item.quantity} x {item.price}$</td>
+                    <td>✖</td>
+                </tr>
+            ))}
+        </table>
+        <h4><br />Total: {total}$</h4>
+    </div>
 );
 
-export default Checkout;
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems,
+    total: selectCartTotal,
+});
+
+export default connect(mapStateToProps)(Checkout);
