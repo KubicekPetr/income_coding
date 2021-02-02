a6e12150ff881602eda4dcaa8bc2824a2176a7ac
KubicekPetr <kubicekpetr@outlook.com>
2021-02-02 22:22:30 +0100

    adding a test card number

--- src/pages/checkout/checkout.page.jsx
+++ src/pages/checkout/checkout.page.jsx
@@ -27,6 +27,10 @@ const Checkout = ({ cartItems, total }) => (
             </tbody>
         </table>
         <h4><br />Total: {total}$</h4>
+        <div>
+            *Please use the following test credit card for payments*<br />
+            4242 4242 4242 4242 - Exp: 01/23 - CVV: 123
+        </div>
         <StripeButton price={total} />
     </div>
 );
