21d758db0e1293dd0f381ed9660ae1f0e81a6051
KubicekPetr <kubicekpetr@outlook.com>
2021-02-27 23:18:51 +0100

    removing redux persist as it does not work as would be expected and causes some unintentional persisted data, which hides bugs, I wil come to that later

--- src/index.js
+++ src/index.js
@@ -14,9 +14,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,4 +1,4 @@
-import { persistCombineReducers } from 'redux-persist';
+import { combineReducers } from 'redux';
 import localStorage from 'redux-persist/lib/storage'; 
 
 import userReducer from './user/user.reducer';
@@ -6,13 +6,7 @@ import cartReducer from './cart/cart.reducer';
 import directoryReducer from './directory/directory.reducer';
 import shopReducer from './shop/shop.reducer';
 
-const persistConfig = {
-    key: 'root',
-    storage: localStorage,
-    whiteList: ['cart'],
-};
-
-export default persistCombineReducers(persistConfig, {
+export default combineReducers({
     user: userReducer,
     cart: cartReducer,
     directory: directoryReducer,
