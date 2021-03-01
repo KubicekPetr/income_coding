29dfd5806168fac5e1ef0ba32a3130dd2c51c729
KubicekPetr <kubicekpetr@outlook.com>
2021-03-01 23:56:42 +0100

    refactoring sign in page with hooks (useState) - from one big nested state object we can create several smaller objects, interesting is that we do not use any construct around async setState when using the last value, like with prevState

--- src/pages/sign-in/sing-in.page.jsx
+++ src/pages/sign-in/sing-in.page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Input from '../../components/input/input.component';
 import Button from '../../components/button/button.component';
@@ -6,93 +6,83 @@ import Warning from '../../components/warning/warning.component';
 
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-class SignIn extends React.Component {
-    constructor() {
-        super();
+const SignIn = () => {
+    const [userCredentials, setUserCredentials] = useState({ email: '', password: '' });
+    const [warning, setWarning] = useState({ active: false, message: '' });
 
-        this.state = {
-            email: '',
-            password: '',
-            warning: {
-                active: false,
-                message: '',
-            },
-        }
-    }
-
-    handleSubmit = async event => {
+    const { email, password } = userCredentials;
+    const { active, message } = warning;
+    
+    const handleSubmit = async event => {
         event.preventDefault();
-
-        const { email, password } = this.state;
 
         try {
             await auth.signInWithEmailAndPassword(email, password);
-        } catch(e) {
+        } catch (e) {
             console.error(e);
             e.code === 'auth/user-not-found' ?
-            this.setState({
-                warning: {
-                    active: true,
-                    message: 'The password is invalid or the user does not have a password.',
-                },
-            }) :
-            this.setState({
-                warning: {
-                    active: true,
-                    message: e.message,
-                },
-            });
+                setWarning({
+                    warning: {
+                        active: true,
+                        message: 'The password is invalid or the user does not have a password.',
+                    },
+                }) :
+                setWarning({
+                    warning: {
+                        active: true,
+                        message: e.message,
+                    },
+                });
         }
 
         // clear inputs after submit
-        this.setState({
+        setUserCredentials({
             email: '',
             password: '',
         });
     }
 
-    handleChange = event => {
+    const handleChange = event => {
         const { value, name } = event.target;
 
-        this.setState({
+        setUserCredentials({
+            ...userCredentials,
             [name]: value,
         });
     }
 
-    render() {
-        return (
-            <div>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
+    return (
+        <div>
+            <h2>I already have an account</h2>
+            <span>Sign in with your email and password</span>
 
-                <form onSubmit={this.handleSubmit}>
-                    <Input
-                        handleChange={this.handleChange}
-                        label="email"
-                        name="email"
-                        type="email"
-                        value={this.state.email}
-                        required />
-                    <Input
-                        handleChange={this.handleChange}
-                        label="password"
-                        name="password"
-                        type="password"
-                        value={this.state.password}
-                        required />
-                    <Button>Sign in</Button>
-                    {
-                        this.state.warning.active && <Warning message={this.state.warning.message} />
-                    }
-                    <p>or</p>
-                    <Button
-                        onClick={signInWithGoogle}
-                        type="button"
-                    >Sign In with Google</Button>
-                </form>
-            </div>
-        );
-    }
+            <form onSubmit={handleSubmit}>
+                <Input
+                    handleChange={handleChange}
+                    label="email"
+                    name="email"
+                    type="email"
+                    value={email}
+                    required />
+                <Input
+                    handleChange={handleChange}
+                    label="password"
+                    name="password"
+                    type="password"
+                    value={password}
+                    required />
+                <Button>Sign in</Button>
+                {
+                    active && <Warning message={message} />
+                }
+                <p>or</p>
+                <Button
+                    onClick={signInWithGoogle}
+                    type="button"
+                >Sign In with Google</Button>
+            </form>
+        </div>
+    );
 }
 
 export default SignIn;
