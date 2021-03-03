e9bcd85ed69be1e150ada356c663c22d1fbf416e
KubicekPetr <kubicekpetr@outlook.com>
2021-03-03 23:00:27 +0100

    rewriting shop page into functional component using useEffect to simulate componentDidMount on fetchCollectionsStartAsync call, because our shop page would have been rerendered everytime app.js changed (which happens bcs of login), the shop page would have been rendered twice, to avoid that we use second parameter in useEffect, and because we know that fetchCollectionsStartAsync prop is never gonna change as it is coming from redux mapsDispatchToProps, we can give it to array to make useEffect call only when fetchCollectionStartAsync change, which is never but componentDidMount

--- src/pages/shop/shop.page.jsx
+++ src/pages/shop/shop.page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,25 +7,21 @@ import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
 import CollectionPageContainer from '../collection/collection.container';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 
-class Shop extends React.Component {
-    componentDidMount() {
-        const { fetchCollectionsStartAsync } = this.props;
+const Shop = ({ match, fetchCollectionsStartAsync }) => {
+    useEffect(() => {
         fetchCollectionsStartAsync();
-    }
+    }, [fetchCollectionsStartAsync]);
 
-    render() {
-        const { match} = this.props;
-        return (
-            <div>
-                <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-                <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+        </div>
+    );
 }
 
 const mapDispatchToProps = dispatch => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
 })
 
 export default connect(null, mapDispatchToProps)(Shop);
